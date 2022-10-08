52d74655c45f4b9577926bbc5d95300b9c1b7dd0
Victor-Rochnyak <victor.rochnyak@gmail.com>
2022-10-09 01:25:33 +0300

    ///

---
+++ js/appi.js
@@ -0,0 +1,14 @@
+const API_KEY = `74cf07cbcff58397c32fe332f07646fa`;
+const image_path =`https://api.themoviedb.org/t/p/w1280`;
+const trailer_path =`http://www.youtube.com/watch?v=`;
+
+
+const input = document.querySelector('.search input');
+const btn = document.querySelector('.search button');
+const mainGridTittle = document.querySelector('.favorites h1');
+const mainGrid = document.querySelector('.favorites .movies-grid');
+
+async function get_movie_by_search (search_term){
+
+}
+// btn.addEventListener('click', add_searched_movies_to_dom)
